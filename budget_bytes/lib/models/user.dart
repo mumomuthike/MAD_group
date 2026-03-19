03d45198f05a5e3ad7ede4113673d38a50968f0d
William Morgan <wmorgan2023@gmail.com>
2026-03-19 12:32:21 -0400

    Removed campus attribute from User class.

--- budget_bytes/lib/models/user.dart
+++ budget_bytes/lib/models/user.dart
@@ -2,14 +2,12 @@
 class User {
   final int? id;
   final String name;
-  final String campus;
   final double weeklyBudget;
   final String dietaryPreferences;
 
   User({
     this.id,
     required this.name,
-    required this.campus,
     required this.weeklyBudget,
     required this.dietaryPreferences,
   });
@@ -18,7 +16,6 @@ class User {
   Map<String, dynamic> toMap() => {
     'user_id': id,
     'name': name,
-    'campus': campus,
     'weekly_budget': weeklyBudget,
     'dietary_preferences': dietaryPreferences,
   };
@@ -27,7 +24,6 @@ class User {
   factory User.fromMap(Map<String, dynamic> map) => User(
     id: map['user_id'],
     name: map['name'],
-    campus: map['campus'],
     weeklyBudget: map['weekly_budget'],
     dietaryPreferences: map['dietary_preferences'],
   );
